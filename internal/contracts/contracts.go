af9f7e87773d983242b1a3cefce6db783ab7a0a3
Michael Whatcott <mike@smarty.com>
2026-06-11 12:59:15 -0600

    Move pipeline struct to contracts.go

--- internal/contracts/contracts.go
+++ internal/contracts/contracts.go
@@ -4,8 +4,25 @@ import (
 	"bytes"
 	"context"
 	"io"
+	"net/http"
 	"time"
 )
+
+type Pipeline struct {
+	// SheddingHTTPWrapper is meant to wrap around any http.Handler that calls SheddingEntrypoint.
+	// It responds with HTTP 503 in the event that the handler is backed up beyond the configured ShedThreshold.
+	SheddingHTTPWrapper func(http.Handler) http.Handler
+
+	// SheddingEntrypoint is a Handler that is meant to be guarded by an admitter (such as SheddingHTTPWrapper).
+	SheddingEntrypoint Handler
+
+	// BlockingEntrypoint is a Handler that will block until the results of the provided work have been durably stored.
+	BlockingEntrypoint Handler
+
+	// Listeners contains each phase of the harness pipeline (serialization, persistence, broadcast, etc.).
+	// Each listener should be invoked on a separate goroutine by a component like github.com/smarty/dominoes.
+	Listeners []Listener
+}
 
 // Interfaces common to many of our external and internal modules
 type (
