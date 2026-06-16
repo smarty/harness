b6000d2f6854276ab736a18f5db8465eec495595
Michael Whatcott <mike@smarty.com>
2026-06-16 11:25:23 -0600

    Establish concept of Snapshot.

--- doc/mysql/schema.sql
+++ doc/mysql/schema.sql
@@ -1,3 +1,13 @@
+CREATE TABLE Snapshots (
+  id               bigint unsigned NOT NULL AUTO_INCREMENT,
+  created          datetime(3)     NOT NULL,
+  high_watermark   bigint unsigned NOT NULL,
+  payload          longblob        NOT NULL,
+  content_type     varchar(127)    NOT NULL DEFAULT '',
+  content_encoding varchar(31)     NOT NULL DEFAULT '',
+  PRIMARY KEY (id)
+);
+
 CREATE TABLE Messages (
     id         bigint unsigned AUTO_INCREMENT NOT NULL,
     dispatched datetime(3)                        NULL,
